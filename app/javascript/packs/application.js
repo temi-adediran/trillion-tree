9224887bebe444cc6d419dd24f5a4f5a593deeee
temi-adediran <fowotop@gmail.com>
2020-07-22 06:14:10 +0100

    complete homepage template integration

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,10 +8,15 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
+
+import jQuery from "jquery";
+import "@fortawesome/fontawesome-free/js/all";
+
+
+
+
+// require("theme/js/plugins.min.js")
 require("theme")
-require("main")
-
-
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -3,7 +3,8 @@ const webpack = require('webpack')
 
 environment.plugins.append('Provide', new webpack.ProvidePlugin({
   $: 'jquery',
-  jQuery: 'jquery'
+  jQuery: 'jquery',
+  Popper: ['popper.js', 'default']
 }))
 
 module.exports = environment
